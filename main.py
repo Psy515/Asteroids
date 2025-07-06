edc939578dd21edaed883f2d1c6554efd5928e29
Psy515 <asuttles515@gmail.com>
2025-07-06 14:05:44 -0500

    Updated Refresh Rate

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import constants
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    clock =  pygame.time.Clock()
+    
+    #dt = 0
+
     print(f"""Starting Asteroids!
 Screen width: {constants.SCREEN_WIDTH}
 Screen height: {constants.SCREEN_HEIGHT}"""
@@ -15,6 +20,7 @@ Screen height: {constants.SCREEN_HEIGHT}"""
                 return
         screen.fill(000000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
