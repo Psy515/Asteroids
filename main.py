731782704597b9f1d40f01739da121873b151424
Psy515 <asuttles515@gmail.com>
2025-07-06 13:55:43 -0500

    Added pygame initial setup

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame
+import constants
 
 def main():
-    print(f"Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    print(f"""Starting Asteroids!
+Screen width: {constants.SCREEN_WIDTH}
+Screen height: {constants.SCREEN_HEIGHT}"""
+            )
+    
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                return
+        screen.fill(000000)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+
     
